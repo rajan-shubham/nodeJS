c48bc4c8fdb6fd997d9e38bab37772143068db73
rajan-shubham <SHUBHAMRAJAN@proton.me>
2025-01-02 22:41:13 +0530

    insertMany documents in db

--- database.js
+++ database.js
@@ -23,6 +23,17 @@ async function main() {
   const insertResult = await collection.insertOne(data);
   console.log("Inserted documents =>", insertResult);
 
+  // Create sample documents
+  const docs = [
+    { stars: 3, categories: ["Bakery", "Sandwiches"], name: "Rising Sun Bakery" },
+    { stars: 4, categories: ["Bakery", "Cafe", "Bar"], name: "Cafe au Late" },
+    { stars: 5, categories: ["Coffee", "Bakery"], name: "Liz's Coffee Bar" },
+    { stars: 3, categories: ["Steak", "Seafood"], name: "Oak Steakhouse" },
+    { stars: 4, categories: ["Bakery", "Dessert"], name: "Petit Cookie" },
+  ];
+  // Insert documents into the restaurants collection
+  const result1 = await collection.insertMany(docs);
+
   // Read
   const findResult = await collection.find({}).toArray();
   console.log("Found documents =>", findResult);
