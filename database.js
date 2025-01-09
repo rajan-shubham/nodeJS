237ad88d58862a29c1938c906adac9da6be87e12
rajan-shubham <SHUBHAMRAJAN@proton.me>
2025-01-09 17:01:00 +0530

    connect mongodb successfully

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 const url =
-  "mongodb+srv://namastedev:ECXK5j0d4OuHZhs6@namastenode.rc211ms.mongodb.net/";
+  "mongodb+srv://shubhamrajan:PLToJSvny58tOtsA@devmilan.su8vc.mongodb.net/";
 
 const client = new MongoClient(url);
 
@@ -20,19 +20,8 @@ async function main() {
     phoneNumber: "987543210",
   };
 
-  const insertResult = await collection.insertOne(data);
-  console.log("Inserted documents =>", insertResult);
-
-  // Create sample documents
-  const docs = [
-    { stars: 3, categories: ["Bakery", "Sandwiches"], name: "Rising Sun Bakery" },
-    { stars: 4, categories: ["Bakery", "Cafe", "Bar"], name: "Cafe au Late" },
-    { stars: 5, categories: ["Coffee", "Bakery"], name: "Liz's Coffee Bar" },
-    { stars: 3, categories: ["Steak", "Seafood"], name: "Oak Steakhouse" },
-    { stars: 4, categories: ["Bakery", "Dessert"], name: "Petit Cookie" },
-  ];
-  // Insert documents into the restaurants collection
-  const result1 = await collection.insertMany(docs);
+  // const insertResult = await collection.insertOne(data);
+  // console.log("Inserted documents =>", insertResult);
 
   // Read
   const findResult = await collection.find({}).toArray();
@@ -43,7 +32,7 @@ async function main() {
 
   // Find all documents with a filter of firstname: Deepika
 
-  const result = await collection.find({ firstname: "Deepika" }).count();
+  const result = await collection.find({ firstname: "Ranveer" }).count();
   console.log("result => ", result);
 
   return "done.";
